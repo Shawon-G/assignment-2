b95ec7a73558fae8ef89ade21d446c46534e504d
ShawonMG <work.shawonmg@gmail.com>
2023-12-22 23:20:31 +0600

    schema-model is created in the model file(without passward hashing)

---
+++ src/app/modules/users/users.model.ts
@@ -0,0 +1,95 @@
+import { Schema, model } from 'mongoose';
+import { Address, Name, Order, User } from './users.interface';
+
+const fullNameSchema = new Schema<Name>({
+  firstName: {
+    type: String,
+    required: [true, 'First name is required'],
+  },
+  middleName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+    required: [true, 'Last name is required'],
+  },
+});
+
+const addressSchema = new Schema<Address>({
+  street: {
+    type: String,
+    required: [true, 'Street is required'],
+  },
+  city: {
+    type: String,
+    required: [true, 'City is required'],
+  },
+  country: {
+    type: String,
+    required: [true, 'Country is required'],
+  },
+});
+
+const orderSchema = new Schema<Order>({
+  productName: {
+    type: String,
+    required: [true, 'Product name is required'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'Price is required'],
+  },
+  quantity: {
+    type: Number,
+    required: [true, 'Quantity is required'],
+  },
+});
+
+const userSchema = new Schema<User>({
+  userId: {
+    type: Number,
+    required: [true, 'User ID is required'],
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: [true, 'Username is required'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+  },
+  fullName: {
+    type: fullNameSchema,
+    required: [true, 'Full name is required'],
+  },
+  age: {
+    type: Number,
+    required: [true, 'Age is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+  hobbies: {
+    type: [String],
+    required: [true, 'Hobbies are required'],
+  },
+  address: {
+    type: addressSchema,
+    required: [true, 'Address is required'],
+  },
+  orders: {
+    type: [orderSchema],
+  },
+});
+
+// Modeling:--------------------------------------------------------
+const UserModel = model<User>('Student', userSchema);
+
+export default UserModel;
